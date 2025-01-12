fa7644caf27e871ab92ba7217b2661eb68cf5334
Andrew Taylor <roo.h.taylor@gmail.com>
2025-01-12 02:22:55 -0500

    2015 day 04 part 1

---
+++ 2015/04/part1/src/main.rs
@@ -0,0 +1,58 @@
+use std::env;
+use std::process;
+use std::io::{self, BufRead};
+use std::fs::File;
+use std::path::Path;
+use md5;
+
+fn main() {
+    // Read the arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {} <inputfilename>", &args[0]);
+        process::exit(1);
+    }
+
+    // The input filename
+    let filename = Path::new(&args[1]);
+
+    // Open the file and read the lines (buffered)
+    let file = match File::open(filename) {
+        Ok(fp) => fp,
+        Err(_) => {
+            println!("Unable to open file!");
+            process::exit(2);
+        },
+    };
+    let lines = io::BufReader::new(file).lines();
+
+    let mut input = String::new();
+    let mut solution = String::new();
+
+    // Iterate over the lines
+    for line in lines.map_while(Result::ok) {
+        // Read only one line - should be the puzzle input
+        input = line.trim().to_string();
+        break;
+    }
+
+    for n in 1..=usize::MAX {
+
+        let test_string = format!("{}{}", &input, n.to_string());
+
+        let digest = md5::compute(&test_string);
+        let digest = format!("{:#?}", digest);
+
+        match &digest[..5] {
+            "00000" => {
+                println!("{}", digest);
+                solution = n.to_string();
+                break;
+            },
+            _ => (),
+        }
+    }
+
+    println!("The smallest number is: {}", solution);
+}
+    
